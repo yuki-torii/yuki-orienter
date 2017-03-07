f3c429c63b52b2f0e59e967b9479c841d5df1790
limichange <limichange@hotmail.com>
2017-03-07 12:11:47 +0800

    📝   - Update simple example

--- examples/simple/app.js
+++ examples/simple/app.js
@@ -1,7 +1,18 @@
 import Orienter from 'yuki-orienter'
 
-const orienter = new Orienter({
-  onOrient: console.log
-})
+const appEl = document.getElementById('app')
 
-console.log(orienter)
+const orienter = new Orienter({
+  onOrient (e) {
+    appEl.innerHTML = `
+      <ul>
+        <li> a : ${e.a} </li>
+        <li> b : ${e.b} </li>
+        <li> g : ${e.g} </li>
+        <li> lon : ${e.lon} (0 - 360)</li>
+        <li> lat : ${e.lat} (-270 - 90) </li>
+        <li> dir : ${e.dir} </li>
+      </ul>
+    `
+  }
+})
